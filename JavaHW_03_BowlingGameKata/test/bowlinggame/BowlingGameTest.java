9d7454a7c3c2b572b1ea9733eae7ad838c9d4475
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:24:25 +0200

    The second test. Slide 25. refactor test code. run test: passed

--- JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -8,18 +8,21 @@ public class BowlingGameTest extends TestCase {
 
     @Override
     protected void setUp() throws Exception {
-        g = new Game(); 
+        g = new Game();
     }
 
     public void testGutterGame() throws Exception {
 
         int n = 20;
         int pins = 0;
+        rollMany(n, pins);
+        assertEquals(0, g.score());
+    }
 
+    private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             g.roll(pins);
         }
-        assertEquals(0, g.score());
     }
 
     public void testAllOnes() throws Exception {
