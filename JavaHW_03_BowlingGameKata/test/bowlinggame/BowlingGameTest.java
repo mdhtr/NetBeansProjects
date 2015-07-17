d746722b11579da502f29200f9b8ef3a9a2ab7c7
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:18:37 +0200

    The second test. Slide 23. satisfy test with new code. run test: passed

--- JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -4,19 +4,28 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 
+    private Game g;
+
+    @Override
+    protected void setUp() throws Exception {
+        g = new Game(); // game creation is not duplicated anymore.
+    }
+
     public void testGutterGame() throws Exception {
-        Game g = new Game();
+
         for (int i = 0; i < 20; i++) {
             g.roll(0);
         }
-        assertEquals(0, g.score()); 
+        assertEquals(0, g.score());
     }
 
     public void testAllOnes() throws Exception {
-        Game g = new Game(); // game creation is duplicated
+
         for (int i = 0; i < 20; i++) { // roll loop is duplicated
-            g.roll(1); 
+            g.roll(1);
         }
-        assertEquals(20, g.score()); // Failed: expected:<20> but was:<0>
+        assertEquals(20, g.score());
     }
+    
+    // Both tests passed.
 }
