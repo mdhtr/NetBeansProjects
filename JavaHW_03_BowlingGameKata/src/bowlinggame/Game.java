e152d1abe56e8c74954ace5a2a43adfde0c94913
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:35:38 +0200

    The third test. Slide 31. analyze code design. code design is wrong

--- JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
+++ JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
@@ -5,7 +5,9 @@ public class Game {
     private int score = 0;
 
     public void roll(int pins) {
-        score += pins;
+        score += pins; /* tempted to use flag to 
+                          remember previous roll.  
+                          So design must be wrong.*/
     }
 
     public int score() {
