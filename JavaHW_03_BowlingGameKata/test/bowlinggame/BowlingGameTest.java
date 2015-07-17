1dd307a8a6d0f5b6d70bfc1e3a796747d76bf1ca
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:42:49 +0200

    The third test. Slide 33. comment out the third test. run test: passed

--- JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -26,12 +26,15 @@ public class BowlingGameTest extends TestCase {
         rollMany(20, 1);
         assertEquals(20, g.score());
     }
+    
+    // Both tests passed.
 
-    public void testOneSpare() throws Exception {
-        g.roll(5);
-        g.roll(5); // spare // ugly comment in test
-        g.roll(3);
-        rollMany(17, 0);
-        assertEquals(16, g.score()); // Failed: expected:<16> but was:<13>
-    }
+//    public void testOneSpare() throws Exception {
+//        g.roll(5);
+//        g.roll(5); // spare // ugly comment in test
+//        g.roll(3);
+//        rollMany(17, 0);
+//        assertEquals(16, g.score()); // Failed: expected:<16> but was:<13>
+//    }
+    
 }
