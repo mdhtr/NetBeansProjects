91cd93f672f5214da6e413eb697737c40f0c681a
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:49:05 +0200

    The third test. Slide 36. refactor code. run test: passed

--- JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
+++ JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
@@ -2,12 +2,10 @@ package bowlinggame;
 
 public class Game {
 
-    private int score = 0;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
         rolls[currentRoll++] = pins;
     }
 
