9f7a13da3bdf4c93331a30fa06df50263acb5627
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:46:45 +0200

    The third test. Slide 35. refactor code. run test: passed

--- JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
+++ JavaHW_03_BowlingGameKata/src/bowlinggame/Game.java
@@ -12,6 +12,10 @@ public class Game {
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+        }
         return score;
     }
 }
