36fc617120a8424603dfca54ad099e479b722bae
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 14:46:06 +0200

    Begin. Slide 10-11.

---
+++ JavaHW_03_BowlingGameKata/src/bowlinggame/BowlingGame.java
@@ -0,0 +1,13 @@
+
+package bowlinggame;
+
+public class BowlingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -0,0 +1,8 @@
+
+package bowlinggame;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+    // No tests found in bowlinggame.BowlingGameTest
+}
