c61154a8c4ffb8526bd6d8d124478024324e7c75
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:10:33 +0200

    The second test. Slide 20. write new test code

--- JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -9,6 +9,14 @@ public class BowlingGameTest extends TestCase {
         for (int i = 0; i < 20; i++) {
             g.roll(0);
         }
-        assertEquals(0, g.score()); // The test passed.
+        assertEquals(0, g.score()); 
+    }
+
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        assertEquals(20, g.score());
     }
 }
