9d164f92104c8d21dceba274ad2e75aeb0a65532
mdhtr <mdhtr@users.noreply.github.com>
2015-07-17 15:05:08 +0200

    The first test. Slide 15. write new test code

--- JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
+++ JavaHW_03_BowlingGameKata/test/bowlinggame/BowlingGameTest.java
@@ -1,10 +1,13 @@
-
 package bowlinggame;
 
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
+
     public void testGutterGame() throws Exception {
-    Game g = new Game(); // The test passed.
-  }
+        Game g = new Game();
+        for (int i = 0; i < 20; i++) {
+            g.roll(0);
+        }
+    }
 }
